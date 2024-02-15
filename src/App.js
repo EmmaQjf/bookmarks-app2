59673ceb4a73829a437f7ee69dd43e97f73ab502
junfengqiao <jfqemma@gmail.com>
2024-02-15 17:05:36 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+
+import styles from './App.module.scss'
+import Nav from './components/Nav/Nav'
+export default function App() {
+   
+    //const [bookmarks, setBookmarks] = useState([])
+    //const [newBookmark, setNewBookmark] = useState({
+
+    return (
+        <>
+        <Nav/>
+        
+        
+        </>
+    )
+}
